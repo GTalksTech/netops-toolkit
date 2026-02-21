db76c70b3df24554bb8d0718a1029c149a4ecd84
G Talks Tech <garrett@gtalkstech.com>
2026-02-21 11:21:25 -0500

    Update show_interfaces_multi.py

--- scripts/netmiko/getting-started/show_interfaces_multi.py
+++ scripts/netmiko/getting-started/show_interfaces_multi.py
@@ -3,21 +3,21 @@ from netmiko import ConnectHandler
 devices = [
     {
         "device_type": "cisco_ios",
-        "host": "ip_address_goes_here",
-        "username": "users_goes_here",
-        "password": "password_goes_here",
+        "host": "192.168.1.10",
+        "username": "admin",
+        "password": "cisco123",
     },
     {
         "device_type": "cisco_ios",
-        "host": "ip_address_goes_here",
-        "username": "users_goes_here",
-        "password": "password_goes_here",
+        "host": "192.168.1.11",
+        "username": "admin",
+        "password": "cisco123",
     },
     {
         "device_type": "cisco_ios",
-        "host": "ip_address_goes_here",
-        "username": "users_goes_here",
-        "password": "password_goes_here",,
+        "host": "192.168.1.12",
+        "username": "admin",
+        "password": "cisco123",
     },
 ]
 
